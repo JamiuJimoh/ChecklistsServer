2afc4802349c9c92e5e83f25d9f70a289c33f7b1
Jamiu Jimoh <hyperjay55@gmail.com>
2024-05-31 21:41:11 +0100

    add database logic to checklists

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"net/http"
+
+	"github.com/JamiuJimoh/checklist/internal/checklist"
 )
 
 func NewServer() (server *http.Server) {
@@ -9,17 +11,17 @@ func NewServer() (server *http.Server) {
 		Addr: "127.0.0.1:8080",
 	}
 
-	http.HandleFunc("GET /checklists", getChecklists)
-	http.HandleFunc("GET /checklists/{id}", getChecklist)
-	http.HandleFunc("POST /checklists", createChecklist)
-	http.HandleFunc("PUT /checklists/{id}", updateChecklist)
-	http.HandleFunc("DELETE /checklists/{id}", deleteChecklist)
+	http.HandleFunc("GET /checklists", checklist.GetChecklists)
+	http.HandleFunc("GET /checklists/{id}", checklist.GetChecklist)
+	http.HandleFunc("POST /checklists", checklist.CreateChecklist)
+	http.HandleFunc("PATCH /checklists/{id}", checklist.UpdateChecklist)
+	http.HandleFunc("DELETE /checklists/{id}", checklist.DeleteChecklist)
 
-	http.HandleFunc("GET /checklists/{checklistID}/items", getChecklistItems)
-	http.HandleFunc("GET /checklists/{checklistID}/items/{id}", getChecklistItem)
-	http.HandleFunc("POST /checklists/{checklistID}/items", createChecklistItem)
-	http.HandleFunc("PUT /checklists/{checklistID}/items/{id}", updateChecklistItem)
-	http.HandleFunc("DELETE /checklists/{checklistID}/items/{id}", deleteChecklistItem)
+	// http.HandleFunc("GET /checklists/{checklistID}/items", getChecklistItems)
+	// http.HandleFunc("GET /checklists/{checklistID}/items/{id}", getChecklistItem)
+	// http.HandleFunc("POST /checklists/{checklistID}/items", createChecklistItem)
+	// http.HandleFunc("PUT /checklists/{checklistID}/items/{id}", updateChecklistItem)
+	// http.HandleFunc("DELETE /checklists/{checklistID}/items/{id}", deleteChecklistItem)
 
 	return server
 }
